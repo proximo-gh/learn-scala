7d875e507882cf74bcf98abb8ba62e41537aef31
proximo <vnlobanovs@gmail.com>
2013-01-19 18:58:35 +0400

    added date to message

--- schat/src/main/scala/ru/proximo/schat/Message.scala
+++ schat/src/main/scala/ru/proximo/schat/Message.scala
@@ -1,11 +1,13 @@
 package ru.proximo.schat
 
+import java.util.Date
+
 /**
  * Created with IntelliJ IDEA.
  * User: proximo
  * Date: 1/18/13
  * Time: 12:51 AM
  */
-class Message(val text:String, from:User, to:User) {
+class Message(val text: String, val from: User, val to: User, val date: Date) {
 
 }
