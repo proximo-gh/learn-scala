384962353bfa8d5b27adfeb80684ce136c52c64a
proximo <vnlobanovs@gmail.com>
2013-01-31 23:51:17 +0400

    Merge Sort class created

---
+++ algo/src/main/scala/ru/proximo/algo/sort/MergeSort.scala
@@ -0,0 +1,13 @@
+package ru.proximo.algo.sort
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: proximo
+ * Date: 1/31/13
+ * Time: 11:50 PM
+ */
+object MergeSort {
+  def sort(input: Array[Int]) : Array[Int] = {
+    input
+  }
+}
