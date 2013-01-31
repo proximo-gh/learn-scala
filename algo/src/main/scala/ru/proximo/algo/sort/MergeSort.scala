6cd7fa53f3464fe3ecec164f545f5be7112da1ae
proximo <vnlobanovs@gmail.com>
2013-02-01 01:13:01 +0400

    Merge Sort class created

--- algo/src/main/scala/ru/proximo/algo/sort/MergeSort.scala
+++ algo/src/main/scala/ru/proximo/algo/sort/MergeSort.scala
@@ -8,6 +8,37 @@ package ru.proximo.algo.sort
  */
 object MergeSort {
   def sort(input: Array[Int]) : Array[Int] = {
+
+    def checkAndSwap(from: Int, to: Int) {
+      if (input(from) > input(to)) {
+        val old = input(from)
+        input(from) = input(to)
+        input(to) = old
+      }
+    }
+
+    def merge(from : Int, to : Int) {
+      var i = from
+      var j = to / 2 + 1
+
+      for (k <- from until to) {
+
+      }
+    }
+
+    def sortPart(from : Int, to : Int) {
+      (to - from) match {
+        case 1 => checkAndSwap(from, to)
+        case 0 =>
+        case _ => {
+          sortPart(from, to / 2)
+          sortPart(to / 2 + 1, to)
+
+          merge(from, to)
+        }
+      }
+    }
+
     input
   }
 }
