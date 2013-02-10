1989cd5ef7055429d125bb8aefe862f1a7fd1036
proximo <vnlobanovs@gmail.com>
2013-02-10 23:37:54 +0400

    Algs + Percolation

--- algs/src/main/java/Percolation.java
+++ algs/src/main/java/Percolation.java
@@ -6,10 +6,13 @@ public class Percolation {
         FULL
     }
 
+    private int n;
+
     private final State[][] grid;
 
     // create N-by-N grid, with all sites blocked
     public Percolation(int N) {
+        n = N;
         if (N <= 0)
             throw new IllegalArgumentException("N must be greater than zero");
 
@@ -22,18 +25,31 @@ public class Percolation {
 
     // open site (row i, column j) if it is not already
     public void open(int i, int j) {
-
+        if(isOpen(i, j))
+            return;
     }
 
     // is site (row i, column j) open?
     public boolean isOpen(int i, int j) {
-        return false;
-    }
+        checkIndexes(i, j);
 
+        State state = grid[i][j];
+        return state != State.BLOCKED;
+    }
 
     // is site (row i, column j) full?
     public boolean isFull(int i, int j) {
-        return false;
+        checkIndexes(i, j);
+
+        State state = grid[i][j];
+
+        return state == State.FULL;
+    }
+
+
+    private void checkIndexes(int i, int j) {
+        if(i < 1 || j < 1 || i >= n || j >= n)
+            throw new IllegalArgumentException();
     }
 
     // does the system percolate?
