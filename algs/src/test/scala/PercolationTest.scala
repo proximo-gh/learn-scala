af142ee46e3dde49573ff0719cf9d9a72626f7cf
proximo <vnlobanovs@gmail.com>
2013-02-11 23:00:39 +0400

    Added tests

---
+++ algs/src/test/scala/PercolationTest.scala
@@ -0,0 +1,18 @@
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+import org.junit.Test
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: proximo
+ * Date: 2/11/13
+ * Time: 10:58 PM
+ */
+@RunWith(classOf[JUnit4])
+class PercolationTest {
+
+  @Test
+  def testOpen(){
+
+  }
+}
