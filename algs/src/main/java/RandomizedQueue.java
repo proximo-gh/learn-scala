4463b0f5c80283f37d3ec99a345adb63b5195f64
proximo <vnlobanovs@gmail.com>
2013-02-17 23:10:08 +0400

    RandomizedQueue via array

--- algs/src/main/java/RandomizedQueue.java
+++ algs/src/main/java/RandomizedQueue.java
@@ -1,6 +1,11 @@
 import java.util.Iterator;
 
 public class RandomizedQueue<Item> implements Iterable<Item> {
+
+    private Object[] items = new Object[0];
+
+    private int size;
+
     // construct an empty randomized queue
     public RandomizedQueue() {
 
@@ -8,27 +13,43 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
 
     // is the queue empty?
     public boolean isEmpty() {
-        return true;
+        return size == 0;
     }
 
     // return the number of items on the queue
     public int size() {
-        return 0;
+        return size;
     }
 
     // add the item
     public void enqueue(Item item) {
+        if (items.length == size) {
+            Object[] newItems = new Object[size + 1];
 
+            for (int i = 0; i < items.length; i++)
+                newItems[i] = items[i];
+
+            items = newItems;
+        }
+
+        items[size] = item;
+
+        size++;
     }
 
     // delete and return a random item
     public Item dequeue() {
-        return null;
+        int index = StdRandom.uniform(size);
+
+        Item item = (Item) items[index];
+
+
+        return item;
     }
 
     // return (but do not delete) a random item
     public Item sample() {
-        return null;
+        return (Item) items[StdRandom.uniform(size)];
     }
 
     // return an independent iterator over items in random order
