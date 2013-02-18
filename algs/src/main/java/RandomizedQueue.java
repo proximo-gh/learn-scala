bef741eb56c8da7ad85da5b60e4cf012b5eb16e5
proximo <vnlobanovs@gmail.com>
2013-02-19 00:18:27 +0400

    optimized dequeue

--- algs/src/main/java/RandomizedQueue.java
+++ algs/src/main/java/RandomizedQueue.java
@@ -45,7 +45,7 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
     }
 
     private int shrinkDelta() {
-        return size / 10;
+        return size / 2;
     }
 
     private void copyItems(int newCapacity) {
