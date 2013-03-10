d3105bd59d4f901f788861c300363f8bd7a10815
proximo <vnlobanovs@gmail.com>
2013-03-11 00:08:25 +0400

    Board constructor

--- algs/src/main/java/Board.java
+++ algs/src/main/java/Board.java
@@ -1,7 +1,7 @@
 public class Board {
 
 
-    private final int[][] blocks;
+    private final int[][] tiles;
     private final int N;
 
     // construct a board from an N-by-N array of blocks
@@ -11,31 +11,49 @@ public class Board {
             throw new IllegalArgumentException("blocks is null");
 
         N = blocks.length;
-        this.blocks = new int[N][N];
+        this.tiles = new int[N][N];
 
         for (int i = 0; i < N; i++)
             for (int j = 0; j < N; j++)
-                this.blocks[i][j] = blocks[i][j];
+                this.tiles[i][j] = blocks[i][j];
     }
 
     // board dimension N
     public int dimension() {
-        return 0;
+        return N;
     }
 
     // number of blocks out of place
     public int hamming() {
-        return 0;
+        int result = 0;
+
+        for (int i = 0; i < N; i++)
+            for (int j = 0; j < N; j++)
+                if (this.tiles[i][j] != i / N + j)
+                    result++;
+
+        return result;
     }
 
     // sum of Manhattan distances between blocks and goal
     public int manhattan() {
-        return 0;
+        int result = 0;
+
+        for (int i = 0; i < N; i++)
+            for (int j = 0; j < N; j++)
+                if (this.tiles[i][j] != i / N + j)
+                    result++;
+
+        return result;
     }
 
     public boolean isGoal() {
-        return false;
+        for (int i = 0; i < N; i++)
+            for (int j = 0; j < N; j++)
+                if (this.tiles[i][j] != i / N + j)
+                    return false;
 
+        return true;
     }
 
     // a board obtained by exchanging two adjacent blocks in the same row
@@ -43,10 +61,26 @@ public class Board {
         return null;
     }
 
-    // does this board equal y?
-    public boolean equals(Object y) {
-        return false;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
 
+        Board board = (Board) o;
+
+        if (N != board.N) return false;
+
+        for (int i = 0; i < N; i++)
+            for (int j = 0; j < N; j++)
+                if (this.tiles[i][j] != board.tiles[i][j])
+                    return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return N;
     }
 
     // all neighboring boards
@@ -56,6 +90,14 @@ public class Board {
 
     // string representation of the board (in the output format specified below)
     public String toString() {
-        return "";
+        StringBuilder s = new StringBuilder();
+        s.append(N + "\n");
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                s.append(String.format("%2d ", tiles[i][j]));
+            }
+            s.append("\n");
+        }
+        return s.toString();
     }
 }
