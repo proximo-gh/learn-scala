f3b59ac01767280513d585e7dd637969c4bb3df6
proximo <vnlobanovs@gmail.com>
2013-03-13 23:02:14 +0400

    kdtrees

--- algs/src/main/java/PointSET.java
+++ algs/src/main/java/PointSET.java
@@ -1,38 +1,63 @@
 public class PointSET {
+    private final SET<Point2D> set = new SET<Point2D>();
+
     // construct an empty set of points
     public PointSET() {
     }
 
     // is the set empty?
     public boolean isEmpty() {
-        return true;
+        return set.isEmpty();
     }
 
     // number of points in the set
     public int size() {
-        return 0;
+        return set.size();
     }
 
     // add the point p to the set (if it is not already in the set)
     public void insert(Point2D p) {
+        set.add(p);
     }
 
     // does the set contain the point p?
     public boolean contains(Point2D p) {
-        return false;
+        return set.contains(p);
     }
 
     // draw all of the points to standard draw
     public void draw() {
+        for (Point2D point2D : set) {
+            StdDraw.point(point2D.x(), point2D.y());
+        }
     }
 
     // all points in the set that are inside the rectangle
     public Iterable<Point2D> range(RectHV rect) {
-        return null;
+        Queue<Point2D> result = new Queue<Point2D>();
+
+        for (Point2D point2D : set) {
+            if (rect.contains(point2D))
+                result.enqueue(point2D);
+        }
+
+        return result;
     }
 
     // a nearest neighbor in the set to p; null if set is empty
     public Point2D nearest(Point2D p) {
-        return null;
+
+        double min = Double.POSITIVE_INFINITY;
+        Point2D nearest = null;
+
+        for (Point2D point2D : set) {
+            double distance = point2D.distanceSquaredTo(p);
+            if (distance < min) {
+                min = distance;
+                nearest = point2D;
+            }
+        }
+
+        return nearest;
     }
 }
