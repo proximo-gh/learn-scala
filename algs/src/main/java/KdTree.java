0be12388e022b1b9a988da53eda49dc0b2b2eb4b
proximo <vnlobanovs@gmail.com>
2013-03-13 23:45:18 +0400

    kdtrees

--- algs/src/main/java/KdTree.java
+++ algs/src/main/java/KdTree.java
@@ -1,4 +1,20 @@
+import java.util.Comparator;
+
 public class KdTree {
+    private static final Comparator<Point2D> BY_X = new Comparator<Point2D>() {
+        @Override
+        public int compare(Point2D o1, Point2D o2) {
+            return Double.compare(o1.x(), o2.x());
+        }
+    };
+
+    private static final Comparator<Point2D> BY_Y = new Comparator<Point2D>() {
+        @Override
+        public int compare(Point2D o1, Point2D o2) {
+            return Double.compare(o1.y(), o2.y());
+        }
+    };
+
     // construct an empty set of points
     public KdTree() {
     }
@@ -34,5 +50,17 @@ public class KdTree {
     // a nearest neighbor in the set to p; null if set is empty
     public Point2D nearest(Point2D p) {
         return null;
+    }
+
+    private static class Node {
+        private Point2D value;
+        private Comparator<Point2D> comparator;
+        private Node left;
+        private Node right;
+
+        private Node(Point2D value, Comparator<Point2D> comparator) {
+            this.value = value;
+            this.comparator = comparator;
+        }
     }
 }
